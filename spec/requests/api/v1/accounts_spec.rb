75890641dcae2b003fe0f3452c95cd389200b9cc
Felipe Vieira <ext.felipe.vieira@farfetch.com>
2022-09-17 14:07:23 +0100

    feat: create account doc

--- spec/requests/api/v1/accounts_spec.rb
+++ spec/requests/api/v1/accounts_spec.rb
@@ -4,97 +4,53 @@ RSpec.describe 'api/v1/accounts', type: :request do
 
   path '/api/v1/accounts' do
 
-    get('list accounts') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
     post('create account') do
-      response(200, 'successful') do
+      tags "Accounts"
+      consumes "application/json"
+      parameter name: :account, in: :body, schema: {
+        type: :object,
+        properties: {
+          user_id: { type: :integer },
+        },
+        required: ["user_id"],
+      }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+      response(201, 'successful') do
+        
+        user = User.first_or_create(name: "Felipe Maciel", email: "felipemrvieira3@gmail.com", 
+          password: "123123", password_confirmation: "123123")
+
+        let(:account) { { user_id: user.id } }
+        run_test! do |response|
+          expect(response).to have_http_status(201)
+          expect(response.content_type).to match(a_string_including("application/json"))
+          expect(response.body).to include_json(
+            user_id: user.id
+          )
         end
-        run_test!
       end
     end
+
   end
 
   path '/api/v1/accounts/{id}' do
-    # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
-
+    
     get('show account') do
-      response(200, 'successful') do
-        let(:id) { '123' }
+      tags "Accounts"
+      consumes "application/json"
+      parameter name: 'id', in: :path, type: :integer, description: 'id', default: 1
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+      response(200, 'successful') do
+
+        account = Account.first_or_create(user_id: User.last)
+        let(:id) { account.id }
+
+        run_test! do |response|
+          expect(response).to have_http_status(200)
+          expect(response.content_type).to match(a_string_including("application/json"))
         end
-        run_test!
       end
     end
 
-    patch('update account') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    put('update account') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    delete('delete account') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
   end
 end
