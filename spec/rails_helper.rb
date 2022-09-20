6e53a2358f7f9ab7a208db0d357e2984b59ceef8
Felipe Vieira <ext.felipe.vieira@farfetch.com>
2022-09-20 11:43:50 +0100

    fix: update rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,7 +66,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
-require 'support/factory_bot'
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
